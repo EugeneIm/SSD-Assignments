601d6338eff199698cfde50bb04e09b79695d541
EugeneIm <immer277@gmail.com>
2021-09-14 12:40:14 -0700

    commit

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,6 +1,7 @@
-print('THis is another test')
+#print('THis is another test')
 
-name = 'SSD'
+name = 'BCIT SSD'
+sub = ':)'
 
 def len_name():
     if len(name) == 4:
@@ -9,4 +10,13 @@ def len_name():
         print('not four')
     return len_name
 
-print(len_name)
+len_name()
+
+def catch_word():
+    if sub in name:
+        print('Inside the sentence')
+    else:
+        print('Not inside the sentence')
+    return catch_word
+
+catch_word()
