fa1a1a46b409e4f190051f8d730f76d82a6b4436
EugeneIm <immer277@gmail.com>
2021-09-14 09:18:08 -0700

    commit

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,1 +1,12 @@
 print('THis is another test')
+
+name = 'SSD'
+
+def len_name():
+    if len(name) == 4:
+        print('four')
+    else:
+        print('not four')
+    return len_name
+
+print(len_name)
